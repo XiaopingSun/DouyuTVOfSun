62dd15434ddf0dc586bfb828f2522d1e19594361
XpSun <teamwork_SunXp@163.com>
2018-08-27 11:52:38 +0800

    初始化项目

---
+++ DouyuTVOfSun/DouyuTVOfSun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouyuTVOfSun
+//
+//  Created by WorkSpace_Sun on 2018/8/27.
+//  Copyright © 2018年 WorkSpace_Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
